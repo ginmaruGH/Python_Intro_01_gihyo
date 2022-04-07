b9e7bb206561d8cda318f38e8b6921863794227c
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-07 17:13:12 +0900

    Chap.1 弾を発射する宇宙船>>宇宙船を操縦する>>宇宙船の移動範囲を制限する

--- jissen/alien_invasion/ship.py
+++ jissen/alien_invasion/ship.py
@@ -28,9 +28,9 @@ class Ship:
     def update(self):
         """左右の移動フラグによって宇宙船の位置を更新する"""
         # 宇宙船のxの値を更新する（rectではない）
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # self.xからrectオブジェクトの位置を更新する
