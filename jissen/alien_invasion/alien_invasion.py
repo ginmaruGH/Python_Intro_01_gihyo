2b34cf16b694bd32281169ccdc874acd34e25076
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-10 01:54:06 +0900

    Chap.2 エイリアン！>>エイリアンの艦隊を編成する>>1列のエイリアンを作成する

--- jissen/alien_invasion/alien_invasion.py
+++ jissen/alien_invasion/alien_invasion.py
@@ -85,9 +85,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """エイリアンの艦隊を作成する"""
-        # 1匹のエイリアンを作成する
+        # 1匹のエイリアンを作成し、1列のエイリアンの数を求める
+        # 各エイリアンの間にはエイリアン1匹分のスペースを空ける
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # 最初の列のエイリアンを作成する
+        for alien_number in range(number_aliens_x):
+            # エイリアンを1匹作成し、列の中に配置する
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """画面上の画像を更新し、新しい画面に切り替える"""
