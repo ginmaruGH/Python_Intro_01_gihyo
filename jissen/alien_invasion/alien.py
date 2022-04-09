6e94af7055364f7a7169636b942e21ebcfbd3611
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-10 05:10:15 +0900

    Chap.2 エイリアン！>>艦隊を動かす>>エイリアンがどちらかの端に達したかを確認する

--- jissen/alien_invasion/alien.py
+++ jissen/alien_invasion/alien.py
@@ -23,7 +23,13 @@ class Alien(Sprite):
         # エイリアンの実際の位置を格納する
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        """エイリアンが画面の端に達した場合は、Trueを返す"""
+        screen_rect = self.screen.get_rect()
+        if (self.rect.right >= screen_rect.right) or (self.rect.left) <= 0:
+            return True
+
     def update(self):
-        """エイリアンを右に移動する"""
-        self.x += self.settings.alien_speed
+        """エイリアンを右または左に移動する"""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
