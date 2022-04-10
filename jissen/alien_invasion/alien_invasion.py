bbaf4c8d8415af156b06bedf05f8b685b656a1ee
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-10 12:42:32 +0900

    Chap.3 ゲームをリセットする>>ゲーム開始ボタンを無効化する

--- jissen/alien_invasion/alien_invasion.py
+++ jissen/alien_invasion/alien_invasion.py
@@ -65,7 +65,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """プレイヤーがPlayボタンをクリックしたら、新規ゲームを開始する"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # ゲームの統計情報をリセットする
             self.stats.reset_stats()
             self.stats.game_active = True
