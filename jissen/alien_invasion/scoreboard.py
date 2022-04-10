e42380aaec9d577a65437c3b944594607c4cea88
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-11 05:02:38 +0900

    Chap.3 得点を表示する>>得点を表示する>>得点を丸める p.81

--- jissen/alien_invasion/scoreboard.py
+++ jissen/alien_invasion/scoreboard.py
@@ -19,7 +19,9 @@ class Scoreboard:
 
     def prep_score(self):
         """得点を描画用の画像に変換する"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        # score_str = str(self.stats.score)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
