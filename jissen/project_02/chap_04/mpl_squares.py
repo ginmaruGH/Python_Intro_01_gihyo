21b485a7574fe82c819c6cf1c04b723dbb5a212b
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-12 18:16:33 +0900

    Chap.4 データを生成する>>簡単な折れ線グラフを描画する>>ラベル線の太さを変更する p.98

--- jissen/project_02/chap_04/mpl_squares.py
+++ jissen/project_02/chap_04/mpl_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# グラフのタイトルと軸のラベルを設定する
+ax.set_title("Square Number", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# 目盛りラベルのスタイルを設定する
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
