37a736c589b61c3237829154ec0803fc6b204436
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-12 18:23:27 +0900

    Chap.4 データを生成する>>簡単な折れ線グラフを描画する>>グラフを修正する p.100

--- jissen/project_02/chap_04/mpl_squares.py
+++ jissen/project_02/chap_04/mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3, 4, 5, 6]
+squares = [1, 4, 9, 16, 25, 36]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # グラフのタイトルと軸のラベルを設定する
 ax.set_title("Square Number", fontsize=24)
