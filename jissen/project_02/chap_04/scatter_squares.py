2331d72f0e701630bf783f6f30359f0ad82f332d
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-14 17:29:52 +0900

    Chap.4 データを生成する>>簡単な折れ線グラフを描画する>>scatter()で複数の点にスタイルを指定して描画する p.103

--- jissen/project_02/chap_04/scatter_squares.py
+++ jissen/project_02/chap_04/scatter_squares.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+# グラフのタイトルと軸ラベルを設定する
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# 目盛りラベルのサイズを設定する
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
