b08364a2142acd5c7f85eefb88f869b7420ae44b
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-14 17:40:54 +0900

    Chap.4 データを生成する>>簡単な折れ線グラフを描画する>>色をカスタマイズする p.106

--- jissen/project_02/chap_04/scatter_squares.py
+++ jissen/project_02/chap_04/scatter_squares.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5, 6]
-y_values = [1, 4, 9, 16, 25, 36]
+# x_values = [1, 2, 3, 4, 5, 6]
+# y_values = [1, 4, 9, 16, 25, 36]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+# ax.scatter(x_values, y_values, s=100)
+# ax.scatter(x_values, y_values, s=10)
+# ax.scatter(x_values, y_values, c='red', s=10)
+ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
 
 # グラフのタイトルと軸ラベルを設定する
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +19,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # 目盛りラベルのサイズを設定する
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# 各軸の範囲を設定する
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
