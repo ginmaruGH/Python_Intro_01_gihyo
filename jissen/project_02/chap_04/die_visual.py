0823abb94361a69826df51c437c34a268aa5a791
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-16 23:34:41 +0900

    Chap.4 データを生成する>>Plotlyでサイコロを転がす>>結果を分析する p.120

--- jissen/project_02/chap_04/die_visual.py
+++ jissen/project_02/chap_04/die_visual.py
@@ -5,8 +5,14 @@ die =Die()
 
 # サイコロを転がし、結果をリストに格納する
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# 結果を分析する
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
