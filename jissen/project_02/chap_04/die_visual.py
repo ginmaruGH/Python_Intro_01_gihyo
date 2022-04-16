a97fad939e05f460bfb4edb85fa15f7c89153a64
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 02:26:30 +0900

    Chap.4 データを生成する>>Plotlyでサイコロを転がす>>ヒストグラムを作成する p.121

--- jissen/project_02/chap_04/die_visual.py
+++ jissen/project_02/chap_04/die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # D6を作成する
@@ -15,4 +18,19 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# print(frequencies)
+
+# 結果を可視化する
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': '結果'}
+y_axis_config = {'title': '発生した回数'}
+my_layout = Layout(
+    title='6面のサイコロを1000回転がした結果',
+    xaxis=x_axis_config, yaxis=y_axis_config
+)
+offline.plot(
+    {'data': data, 'layout': my_layout},
+    filename='jissen/project_02/chap_04/d6.html'
+)
