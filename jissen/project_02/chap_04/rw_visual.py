f39c41d7b2445debd856c45d8e95c4876b780f6b
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-16 19:17:08 +0900

    Chap.4 データを生成する>>ランダムウォーク>>ランダムウォークにスタイルを設定する>>点に色を付ける p.113

--- jissen/project_02/chap_04/rw_visual.py
+++ jissen/project_02/chap_04/rw_visual.py
@@ -12,7 +12,9 @@ while True:
     # ランダムウォークの点を描画する
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    # ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     plt.show()
 
     keep_running = input("別のランダムウォークを生成する？（y/n）>> ")
