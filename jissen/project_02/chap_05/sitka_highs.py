2bc9542081993aef179b3b26c1d5b53bf6b3fda4
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 08:53:37 +0900

    Chap.5 データをダウンロードする>>CSVファイル形式>>日付を描画する p.136

--- jissen/project_02/chap_05/sitka_highs.py
+++ jissen/project_02/chap_05/sitka_highs.py
@@ -1,4 +1,7 @@
 import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
 
 filename = 'jissen/project_02/chap_05/data/sitka_weather_07-2018_simple.csv'
 
@@ -6,10 +9,27 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # ファイルから最高気温を取得する
-    highs = []
+    # ファイルから日付と最高気温を取得する
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
-print(highs)
+# 最高気温のグラフを描画する
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+# グラフにフォーマットを指定する
+plt.title(
+    "Daily high temperatures, July 2018",
+    fontsize=24
+)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
