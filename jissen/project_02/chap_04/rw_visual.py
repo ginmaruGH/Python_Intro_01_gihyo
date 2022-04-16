93de7f7986c6c4b795e4934d48bbd02b9e0c1a26
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-16 19:28:28 +0900

    Chap.4 データを生成する>>ランダムウォーク>>ランダムウォークにスタイルを設定する>>描画する点を追加する p.116

--- jissen/project_02/chap_04/rw_visual.py
+++ jissen/project_02/chap_04/rw_visual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 while True:
 
     # ランダムウォークを作成する
-    rw = RandomWalk()
+    rw = RandomWalk(500_000)
     rw.fill_walk()
 
     # ランダムウォークの点を描画する
@@ -14,15 +14,15 @@ while True:
     fig, ax = plt.subplots()
     # ax.scatter(rw.x_values, rw.y_values, s=15)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # 開始点と終了点を強調する
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     # 軸を削除する
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
 
     plt.show()
 
