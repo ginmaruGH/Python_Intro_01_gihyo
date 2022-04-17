986f5e6f0846ba2bb8713092d67de3d982760ec0
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 21:00:36 +0900

    Chap.5 データをダウンロードする>>地峡全体のデータセットを地図に描画する（JSON）>>すべての地震のリストを作成する p.149

--- jissen/project_02/chap_05/eq_explore_data.py
+++ jissen/project_02/chap_05/eq_explore_data.py
@@ -6,6 +6,8 @@ filename = 'jissen/project_02/chap_05/data2/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'jissen/project_02/chap_05/data2/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+# readable_file = 'jissen/project_02/chap_05/data2/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
