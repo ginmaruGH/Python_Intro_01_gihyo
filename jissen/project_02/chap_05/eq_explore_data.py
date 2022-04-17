35b7b5dec05bf670e1ac68a5a7d7c6da16e02a92
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 21:19:33 +0900

    Chap.5 データをダウンロードする>>地峡全体のデータセットを地図に描画する（JSON）>>位置データを取り出す p.150

--- jissen/project_02/chap_05/eq_explore_data.py
+++ jissen/project_02/chap_05/eq_explore_data.py
@@ -9,13 +9,22 @@ with open(filename) as f:
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
 
-mags = []
+# マグニチュード、位置データをを取り出す
+# 経度（longitude）、経度（latitude）
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
 
+# すべての地震のリストを作成する
 # readable_file = 'jissen/project_02/chap_05/data2/readable_eq_data.json'
 # with open(readable_file, 'w') as f:
 #     json.dump(all_eq_data, f, indent=4)
