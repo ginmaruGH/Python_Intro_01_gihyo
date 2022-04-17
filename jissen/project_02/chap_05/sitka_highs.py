f257c2246e0d5772452e61824ecab972a766d76e
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 09:03:24 +0900

    Chap.5 データをダウンロードする>>CSVファイル形式>>グラフ内の領域に陰影を付ける p.139

--- jissen/project_02/chap_05/sitka_highs.py
+++ jissen/project_02/chap_05/sitka_highs.py
@@ -22,8 +22,9 @@ with open(filename) as f:
 # 最高気温と最低気温をグラフに描画する
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
-ax.plot(dates, lows, c='blue')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # グラフにフォーマットを指定する
 plt.title(
