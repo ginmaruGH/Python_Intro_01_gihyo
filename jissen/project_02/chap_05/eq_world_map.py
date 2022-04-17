b72c4f3fdd2561d4503b1d2215a9661b5079dc71
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 22:18:37 +0900

    hap.5 データをダウンロードする>>地球全体のデータセットを地図に描画する（JSON）>>ホバーテキストを追加する p.157

--- jissen/project_02/chap_05/eq_world_map.py
+++ jissen/project_02/chap_05/eq_world_map.py
@@ -15,14 +15,16 @@ print(len(all_eq_dicts))
 
 # マグニチュード、位置データをを取り出す
 # 経度（longitude）、経度（latitude）
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # 地震の地図
 # data = [Scattergeo(lon=lons, lat=lats)]
@@ -30,6 +32,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
