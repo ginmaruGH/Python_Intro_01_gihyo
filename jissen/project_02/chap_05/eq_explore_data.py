60447e068180852694ae097a9a3606996c110242
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-17 21:05:38 +0900

    Chap.5 データをダウンロードする>>地峡全体のデータセットを地図に描画する（JSON）>>マグニテュードを取り出す p.150

--- jissen/project_02/chap_05/eq_explore_data.py
+++ jissen/project_02/chap_05/eq_explore_data.py
@@ -8,6 +8,14 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
+
 # readable_file = 'jissen/project_02/chap_05/data2/readable_eq_data.json'
 # with open(readable_file, 'w') as f:
 #     json.dump(all_eq_data, f, indent=4)
