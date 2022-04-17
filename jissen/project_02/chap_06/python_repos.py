76d9b8642bc518355ee9045172e9a88f44348783
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2022-04-18 03:06:58 +0900

    Chap.6 APIを取り扱う>>Web APIを使う>>レスポンスの辞書を処理する p.165

--- jissen/project_02/chap_06/python_repos.py
+++ jissen/project_02/chap_06/python_repos.py
@@ -10,4 +10,23 @@ print(f"ステータスコード: {r.status_code}")
 response_dict = r.json()
 
 # 結果を処理する
-print(response_dict.keys())
+# print(response_dict.keys())
+print(f"全リポジトリ数: {response_dict['total_count']}")
+
+# リポジトリに関する情報を調べる
+repo_dicts = response_dict['items']
+print(f"情報が返されたリポジトリの数: {len(repo_dicts)}")
+
+# 1つ目のリポジトリを調査する
+repo_dict = repo_dicts[0]
+print("\n1つ目のリポジトリの情報の抜粋")
+print(f"\nキーの数: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+print(f"名前: {repo_dict['name']}")
+print(f"所有者: {repo_dict['owner']['login']}")
+print(f"スターの数: {repo_dict['stargazers_count']}")
+print(f"リポジトリURL: {repo_dict['html_url']}")
+print(f"作成日時: {repo_dict['created_at']}")
+print(f"最終更新日時: {repo_dict['updated_at']}")
+print(f"説明文: {repo_dict['description']}")
